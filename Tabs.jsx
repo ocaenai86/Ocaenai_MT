a3b39e0d86d1357c45e2669e3be2d026bcffec48
ocaenai86 <ocaenai.86@gmail.com>
2026-08-20 01:27:15 +0330

    Add Tabs component for tab navigation

--- Tabs.jsx
+++ Tabs.jsx
@@ -1,1 +1,15 @@
-
+export default function Tabs({ active, setActive }) {
+  return (
+    <div className="tabs">
+      {["html", "css", "js"].map(key => (
+        <button
+          key={key}
+          className={active === key ? "tab active" : "tab"}
+          onClick={() => setActive(key)}
+        >
+          {key.toUpperCase()}
+        </button>
+      ))}
+    </div>
+  );
+}
