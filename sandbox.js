22f7b701916560cfc9106913e10241406f3e81fc
ocaenai86 <ocaenai.86@gmail.com>
2026-08-20 01:26:07 +0330

    Add runSandbox function to execute HTML, CSS, JS

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,22 @@
+export function runSandbox(html, css, js, onLog) {
+  const iframe = document.getElementById("ocaenai-preview");
 
+  const consoleProxy = {
+    log: (...args) => onLog(args.join(" "))
+  };
+
+  const script = `
+    const console = ${JSON.stringify(consoleProxy)};
+    try { ${js} } catch(e) { console.log(e.message); }
+  `;
+
+  const doc = `
+    <!DOCTYPE html>
+    <html>
+      <head><style>${css}</style></head>
+      <body>${html}<script>${script}<\/script></body>
+    </html>
+  `;
+
+  iframe.srcdoc = doc;
+}
