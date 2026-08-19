2d086da3ef190a5b28606f6381878e1da6da0465
ocaenai86 <ocaenai.86@gmail.com>
2026-08-20 01:25:28 +0330

    Add Console component to display logs

--- Console.jsx
+++ Console.jsx
@@ -1,1 +1,11 @@
+import React from "react";
 
+export default function Console({ logs }) {
+  return (
+    <div className="console">
+      {logs.map((log, i) => (
+        <div key={i} className="console-line">{log}</div>
+      ))}
+    </div>
+  );
+}
