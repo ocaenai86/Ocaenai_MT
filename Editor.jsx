1df2e70fd7656e5d8410ec0434c50dc85f2b6f77
ocaenai86 <ocaenai.86@gmail.com>
2026-08-20 01:24:36 +0330

    Add CodeMirror editor component
    Implement a CodeMirror editor component with support for HTML, CSS, and JavaScript languages.

--- Editor.jsx
+++ Editor.jsx
@@ -1,1 +1,31 @@
+import React, { useEffect, useRef } from "react";
+import { EditorView, basicSetup } from "codemirror";
+import { html } from "@codemirror/lang-html";
+import { css } from "@codemirror/lang-css";
+import { javascript } from "@codemirror/lang-javascript";
 
+export default function Editor({ code, onChange, lang }) {
+  const editorRef = useRef(null);
+
+  useEffect(() => {
+    const view = new EditorView({
+      doc: code,
+      extensions: [
+        basicSetup,
+        lang === "html" && html(),
+        lang === "css" && css(),
+        lang === "js" && javascript(),
+        EditorView.updateListener.of(update => {
+          if (update.docChanged) {
+            onChange(update.state.doc.toString());
+          }
+        })
+      ],
+      parent: editorRef.current
+    });
+
+    return () => view.destroy();
+  }, [lang]);
+
+  return <div ref={editorRef} className="editor-container" />;
+}
