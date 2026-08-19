cf47519c4d23083a756c058b244945fbe7215f7d
ocaenai86 <ocaenai.86@gmail.com>
2026-08-20 01:27:52 +0330

    Add StatusBar component to display status information

--- StatusBar.jsx
+++ StatusBar.jsx
@@ -1,1 +1,10 @@
-
+export default function StatusBar({ lang, lines, chars, saveState }) {
+  return (
+    <div className="status-bar">
+      <span>{lang.toUpperCase()}</span>
+      <span>{lines} خط</span>
+      <span>{chars} کاراکتر</span>
+      <span>{saveState === "saving" ? "در حال ذخیره…" : "ذخیره شد"}</span>
+    </div>
+  );
+}
